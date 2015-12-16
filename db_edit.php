eb57b1f8ab9a02b4f674f2fdccba1c4fc8ec2590
Nicolas Rollier <nicolas@rollier.be>
2015-12-16 19:30:03 +0100

    Remove whitespace and update libraries

--- db_edit.php
+++ db_edit.php
@@ -8,13 +8,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<title>Edit Bookmarks</title>	
-	<link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+	<title>Edit Bookmarks</title>
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
   </head>
 <body>
-<?php 
+<?php
 	if($_POST['submit'] == 1) {
 		if($_POST['name'] == '') {
 			echo ''.$warning.'';
@@ -27,7 +27,7 @@
 		if($error != true) {
 	
 		$query = "UPDATE links SET name = '$_POST[name]', url = '$_POST[url]', date = '$_POST[date]' WHERE id = '$_POST[id]'";
-		$result = mysqli_query($dbc, $query); 
+		$result = mysqli_query($dbc, $query);
 		}
 	}
  ?>
@@ -35,7 +35,7 @@
 <div class="container" id="list">
 <div class="page-header">
   <h1>Edit Bookmarks</h1>
-  <p class="lead">Basic interface to <strong>update</strong> bookmarks stored in a database  
+  <p class="lead">Basic interface to <strong>update</strong> bookmarks stored in a database
   <a href="db_edit.php" class="none"><sup><i class="fa fa-refresh"></i></sup></a></p>
 </div>
 
@@ -43,18 +43,18 @@
 <div class="col-md-5">
 	<p>Display all active <b>links</b>:<br>
 	<i>click the icon to edit...</i></p>
-		<?php 
+		<?php
 			$result = mysqli_query($dbc, "SELECT * FROM links WHERE type = 1 ORDER BY date DESC");
 			while ($output = mysqli_fetch_assoc($result)) {
-				echo '<a href="?id='.$output['id'].'" class="none">'.$link.'<a href="'.$output['url'].'">'.$output['name'].'</a><br />'; } ?> 
+				echo '<a href="?id='.$output['id'].'" class="none">'.$link.'<a href="'.$output['url'].'">'.$output['name'].'</a><br />'; } ?>
 </div>
 
 <div class="col-md-5 col-md-offset-1">
-<?php if($_GET['id']) { 
+<?php if($_GET['id']) {
 	$query = "SELECT * FROM links WHERE id = '$_GET[id]'";
-	$result = mysqli_query($dbc, $query); 	
+	$result = mysqli_query($dbc, $query);
 	$load = mysqli_fetch_assoc($result);
-	?> 
+	?>
 		<form method="post" action="db_edit.php?id=<?php echo $load['id'];?>" role="form">
 		  <div class="form-group">
 			<p> Edit bookmark <b>description</b> :</p>
@@ -67,13 +67,13 @@
 		  <button type="submit" name="submit" class="btn btn-default pull-right" value="1">Update bookmark</button><br />
 		  <input type="hidden" name="id" value="<?php echo $load['id'];?>" />
 		  <input type="hidden" name="date" value="<?php echo ''.$stamp.''; ?>" />
-		</form>	
-	<?php 
+		</form>
+	<?php
 		} ?>
 </div>
 </div>
 
-<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 </body>
 </html>
--- db_list.php
+++ db_list.php
@@ -7,9 +7,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<title>List Bookmarks</title>	
-	<link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+	<title>List Bookmarks</title>
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="assets/css/style.css">
   </head>
 <body>
@@ -21,16 +21,16 @@
   <a href="db_list.php" class="none"><sup><i class="fa fa-refresh"></i></sup></a></p>
 </div>
 
-<div class="row" id="list" >
+<div class="row" id="list">
 	<div class="col-md-4">
-	<?php 
+	<?php
 	// Replace 'host','username' and 'password' with the proper values
-	mysql_connect('host','username','password') or die(mysql_error());	
+	mysql_connect('host','username','password') or die(mysql_error());
 
 	// Replace 'database' with the proper value
  	mysql_select_db('database') or die(mysql_error());
 
-	  
+
 	class netstedCategories {
 		public static function generateMenu($categories,$level){
 			echo '<ul class="overview">';
@@ -51,9 +51,9 @@
 				$categories[]=$row;
 			}
 			$nestedCategories=netstedCategories::getChildren($categories,0);
-			netstedCategories::generateMenu($nestedCategories,0); 
+			netstedCategories::generateMenu($nestedCategories,0);
 		}
-		public static function hasChildren($categories,$parent_id){ 
+		public static function hasChildren($categories,$parent_id){
 			foreach($categories as $category){
 				if($category->parent==$parent_id) return true;
 			}
@@ -62,10 +62,10 @@
 	 	public static function getChildren($categories ,$parent_id){
 			$temp=array();
 			foreach($categories as $category){
-				if($category->parent==$parent_id){ 
+				if($category->parent==$parent_id){
 					if(netstedCategories::hasChildren($categories,$category->id)){
 						$category->children=netstedCategories::getChildren($categories,$category->id);
-					} 
+					}
 					$temp[]=$category;
 					}
 			}
@@ -75,18 +75,20 @@
 	netstedCategories::buildMenu();
 	?>
 	</div>
-</div> <!--end of row--> 
+</div> <!--end of row-->
 </div> <!--end of container-->
 
-<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script><script type='text/javascript'>//<![CDATA[ 
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+<script type='text/javascript'>
+//<![CDATA[
 	$(window).load(function(){
-	$('.overview ul').hide();	
+	$('.overview ul').hide();
 	$('.toggle').click(function() {
 	    $(this).find('ul').slideToggle();
 	});
-});//]]>  
-</script>  
+});//]]>
+</script>
 
 </body>
 </html>
